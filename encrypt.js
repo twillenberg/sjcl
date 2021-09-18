14a521ff3efecde5b420c8a0bb8773fdfc6afd85
Tony Willenberg <tony.willenberg@gmail.com>
2021-09-18 17:26:03 +0200

    introduced objects

--- encrypt.js
+++ encrypt.js
@@ -1,11 +1,9 @@
 import crypto from 'sjcl'
 
-let prik = "12345"
-console.log('Private Key:\t',prik)
-let pt = "The quick brown fox jumped over the lazy dog."
-console.log('Plaintext:\t',pt)
+let privateKey = "67890";
+let payload = { plain:"The quick brown fox jumped over the lazy dog!", key:privateKey, cipher:""};
 
-let ct = crypto.encrypt(prik,pt)
-console.log("Ciphertext:\t",ct)
+console.log("PAYLOAD.plain:\t", payload.plain);
+console.log("PAYLOAD.key:\t", payload.key);
+console.log("PAYLOAD.cipher:\t", payload.cipher=crypto.encrypt(payload.key,payload.plain));
 
-// This is a test comment.
