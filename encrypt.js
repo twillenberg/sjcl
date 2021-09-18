f8df789e710256e96b395b703dc4980cb1c40c32
Tony Willenberg <tony.willenberg@gmail.com>
2021-09-18 22:09:02 +0200

    formatted package.json

--- encrypt.js
+++ encrypt.js
@@ -11,4 +11,4 @@ console.log("PAYLOAD.plain:\t", payload.plain);
 console.log("PAYLOAD.key:\t", payload.key);
 console.log("PAYLOAD.cipher:\t", payload.cipher=crypto.encrypt(payload.key,payload.plain));
 console.log("PLAIN:\t",payload.data);
-console.log("HSHA256:\t",payload.sig);
+console.log("SHA256:\t",payload.sig);
