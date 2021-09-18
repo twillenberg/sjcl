3f30568a19fd7c014479cd22e4eaebd70284cd98
Tony Willenberg <tony.willenberg@gmail.com>
2021-09-18 17:56:47 +0200

    added use of the hash class

--- encrypt.js
+++ encrypt.js
@@ -1,9 +1,14 @@
 import crypto from 'sjcl'
 
+let empty = "";
 let privateKey = "67890";
-let payload = { plain:"The quick brown fox jumped over the lazy dog!", key:privateKey, cipher:""};
+let payload = { plain:"The quick brown fox jumped over the lazy dog!", key:privateKey, cipher:empty, sig:empty};
+
+let sig = crypto.hash.sha256()
+payload.sig = sig.hash(payload.plain);
 
 console.log("PAYLOAD.plain:\t", payload.plain);
 console.log("PAYLOAD.key:\t", payload.key);
 console.log("PAYLOAD.cipher:\t", payload.cipher=crypto.encrypt(payload.key,payload.plain));
-
+console.log("PLAIN:\t",payload.data);
+console.log("HSHA256:\t",payload.sig);
