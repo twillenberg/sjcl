dc88cd730a9a3e20328d42a7d07846f21724530e
Tony Willenberg <tony.willenberg@gmail.com>
2021-09-19 21:57:56 +0200

    debugging

--- encrypt.js
+++ encrypt.js
@@ -1,16 +1,11 @@
 import crypto from 'sjcl'
 
-
+console.log("here");
 let privateKey = "67890";
-let payload = { 
-     plain:"The quick brown fox jumped over the lazy dog!", 
-     key:privateKey, 
-     cipher:"", 
-     sig:""
-};
-
+let payload = {plain:"The quick brown fox jumped over the lazy dog!", key:privateKey, cipher:"", sig:""};
 let sig = crypto.hash.sha256()
 payload.sig = sig.hash(payload.plain);
+
 console.log("PAYLOAD.plain:\t", payload.plain);
 console.log("PAYLOAD.key:\t", payload.key);
 console.log("PAYLOAD.cipher:\t", payload.cipher=crypto.encrypt(payload.key,payload.plain));
