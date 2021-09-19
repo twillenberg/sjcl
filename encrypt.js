26906eb0283f6f8251c39ba9bc26898a67dda291
Tony Willenberg <tony.willenberg@gmail.com>
2021-09-19 11:10:37 +0200

    formatted object creation code

--- encrypt.js
+++ encrypt.js
@@ -1,8 +1,13 @@
 import crypto from 'sjcl'
 
-let empty = "";
+
 let privateKey = "67890";
-let payload = { plain:"The quick brown fox jumped over the lazy dog!", key:privateKey, cipher:empty, sig:empty};
+let payload = { 
+     plain:"The quick brown fox jumped over the lazy dog!", 
+     key:privateKey, 
+     cipher:"", 
+     sig:""
+};
 
 let sig = crypto.hash.sha256()
 payload.sig = sig.hash(payload.plain);
@@ -10,5 +15,5 @@ payload.sig = sig.hash(payload.plain);
 console.log("PAYLOAD.plain:\t", payload.plain);
 console.log("PAYLOAD.key:\t", payload.key);
 console.log("PAYLOAD.cipher:\t", payload.cipher=crypto.encrypt(payload.key,payload.plain));
-console.log("PLAIN:\t",payload.data);
+console.log("PLAIN:\t",payload.plain);
 console.log("SHA256:\t",payload.sig);
